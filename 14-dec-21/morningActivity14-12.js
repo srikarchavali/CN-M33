dd3c59f9bc6a2bc5e999498b57ebcb754a4fcf14
srikar chavali <chavalisrikar@gmail.com>
2021-12-17 15:19:48 +0000

    revision 0

---
+++ 14-dec-21/morningActivity14-12.js
@@ -0,0 +1,46 @@
+// // Activity 1
+// console.log('ACTIVITY-1');
+// let name = 'sam';
+// let age= 25;
+// let favColor = 'blue';
+// console.log(`Hi my name is ${name}. I am ${age} and My favourite color is ${favColor}.`);
+// // Strech
+// name = 'tom';
+// age = 28;
+// favColor = 'red';
+// console.log(`Hi my name is ${name}. I am ${age} and My favourite color is ${favColor}.\n`);
+
+// // Activity 2
+// console.log('ACTIVITY-2');
+// let breakfast = 'bread';
+// let lunch = 'pizza';
+// let dinner = 'rice';
+// console.log(`I had ${breakfast} for my breakfast, ${lunch} for my luch and ${dinner} for my dinner`);
+// // Strech
+// breakfast = 'eggs';
+// lunch = 'burger';
+// dinner = 'pudding';
+// console.log(`I had ${breakfast} for my breakfast, ${lunch} for my luch and ${dinner} for my dinner. \n`);
+
+// //Activity-3
+// console.log('ACTIVITY-3');
+// let birthday = new Date('03/27/2022');
+// let today = new Date();
+// let diff = birthday.getTime()-today.getTime();
+// console.log(`No of days to my birthday is ${Math.floor(diff/(1000*3600*24))}. \n`);
+
+//Activity-4
+console.log('ACTIVITY-4 \n');
+let space1 = 'o'; let space2 = 'o'; let space3 = 'o'; let space4 = 'o'; let space5 = 'o'; 
+let space6 = 'x'; let space7 = 'x'; let space8 = 'x'; let space9 = 'x';
+console.log("     |     |     ");
+console.log(` ${space6}   |  ${space1}  |     `);
+console.log("     |     |     ");
+console.log("------------------");
+console.log("     |     |     ");
+console.log(` ${space7}   |  ${space8}  |     `);
+console.log("     |     |     ");
+console.log("------------------");
+console.log("     |     |     ");
+console.log(` ${space3}   |     |     `);
+console.log("     |     |     ");
