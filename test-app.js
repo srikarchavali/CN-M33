46205591668d3dea4dac653738bd9aae1a119a6c
srikar chavali <chavalisrikar@gmail.com>
2021-12-17 14:35:10 +0000

    first commitment

---
+++ test-app.js
@@ -0,0 +1,11 @@
+const dash = ["     |   |       ","---------------"];
+
+for(let i=0; i<2; i++){
+    for (let j = 0; j < 3; j++) {
+        console.log(dash[0]);
+  }
+    console.log(dash[1]);
+}
+for (let j = 0; j < 3; j++) {
+    console.log(dash[0]);
+}
